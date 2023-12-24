0b6ec0eb5f9b6db09f7225778240e168848bf1d2
Ahmed_Dadousha <bioshock44matrix44@gmail.com>
2023-12-24 12:31:15 +0200

    Fix: Camera Rotation and movment

--- Scripts/y_bot.gd
+++ Scripts/y_bot.gd
@@ -7,32 +7,23 @@ const SPEED = 4.0
 const MAX_SPEED: int = 7
 const JUMP_VELOCITY = 4.5
 const SENS: float = .5
-
+var angle 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var is_running: bool = false
-
+var dir
 #
-@export var speed: float = 5.0
-@export var acceleration: float = 4.0
-@export var jump_speed: float = 8.0
-@export var rotation_speed: float = 12.0
-@export var mouse_sensitivity: float = 0.0015
-
-var jumping: bool = false
+@export var mouse_sensitivity: float = 0.0020
 @onready var spring_arm: SpringArm3D = $SpringArm3D
-@onready var rig: Node3D = $visuals
 
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	dir = Vector3.BACK.rotated(Vector3.UP, spring_arm.global_transform.basis.get_euler().y)
 
 func _input(event):
-		if is_running:
-				var tween: Tween = create_tween()
-				tween.tween_property(spring_arm, "rotation",Vector3(0,0,0), .2).from_current()
-				
 		if event is InputEventMouseMotion:
+			
 			if is_running:
 				rotate_y(deg_to_rad(-event.relative.x * SENS))
 			else:
@@ -54,10 +45,6 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("run"):
 		is_running = !is_running
 
-	# Handle jump.
-	#if Input.is_action_just_pressed("jump") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("left", "right", "forward", "back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
@@ -65,21 +52,26 @@ func _physics_process(delta):
 	if is_running:
 		# Get new run direction
 		var  run_direction = (transform.basis * Vector3(0,0,-1)).normalized()
+		var run_dir = run_direction.rotated(Vector3.UP, spring_arm.transform.basis.get_euler().y).normalized()
+		
 		if animation_player.current_animation != "RunningWithoutWeapon/mixamo_com":
 			animation_player.play("RunningWithoutWeapon/mixamo_com")
 			
-		velocity.x = run_direction.x * MAX_SPEED
-		velocity.z = run_direction.z * MAX_SPEED
-		visuals.look_at(position + run_direction)
-		#visuals.rotation = Vector3($SpringArm3D.rotation.x, $SpringArm3D.rotation.y, 0)
+		velocity.x = run_dir.x * MAX_SPEED
+		velocity.z = run_dir.z * MAX_SPEED
+		visuals.look_at(position + run_dir)
+		
 	if direction:
+		
+		dir = direction.rotated(Vector3.UP, spring_arm.transform.basis.get_euler().y).normalized()
+		
 		is_running = false
 
 		if animation_player.current_animation != "WalkingWithoutWeapon/mixamo_com":
 			animation_player.play("WalkingWithoutWeapon/mixamo_com")
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-		visuals.look_at(position + direction)
+		velocity.x = dir.x * SPEED
+		velocity.z = dir.z * SPEED
+		visuals.look_at(position + dir)
 		
 	elif !direction && !is_running:
 		if animation_player.current_animation != "IdleWithoutWeapon/mixamo_com":
